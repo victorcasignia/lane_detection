3893b511333700fa54d97739b3a0ec94d0cfc4c9
victorcasignia <victorcasignia@gmail.com>
2018-03-07 00:07:48 -0800

    first commit

--- src/calibrate.py
+++ src/calibrate.py
@@ -37,11 +37,4 @@ def undistort(img, mtx, dist):
 	return dst;
 
 
-ret, mtx, dist, rvecs, tvecs = calibrate("data/camera_cal/", 6, 9)
 
-img = cv2.imread("data/camera_cal/calibration4.jpg")
-
-ds = undistort(img, mtx, dist)
-
-cv2.imshow('img',ds)
-cv2.waitKey(0)
